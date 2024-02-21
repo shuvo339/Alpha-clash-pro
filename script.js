42d14cbd9013ab90ae3380efb401bde021c05e44
shuvo339 <shubhroshafiullah@gmail.com>
2024-02-21 15:22:57 +0600

    color fixed

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // }
 
 const audio = new Audio();
-
+let isGamePlayOn = false;
 function continueGame(){
     const alphabet = getARandomAlphabet();
     const currentAlphabet = document.getElementById('current-alphabet');
@@ -18,25 +18,38 @@ function play(){
     hideElementById('home');
     hideElementById('score-section');
     unhideElementById('playground');
-
+    
     setUpdatedPointById('life', 5);
     setUpdatedPointById('score', 0);
+
+    isGamePlayOn = true;
+
     continueGame();
+
+    
 }
 
 function gameOver(){
     hideElementById('playground');
     unhideElementById('score-section');
-
+    
     const finalScore = document.getElementById('score').innerText;
     // console.log(finalScore);
     const currentText = getElementTextById('current-alphabet');
     removeBgColorById(currentText);
     setUpdatedPointById('final-score', finalScore);
+
+    isGamePlayOn = false;
+    const artBoard = document.getElementById('art-board');
+    artBoard.style.background= `linear-gradient(rgba(255,255,255,0.7) 100%, red)`;
 }
 
 
 function handledByKeyboard(e){
+    if(isGamePlayOn===false){
+        return;
+    }
+
     const playerPressed = e.key;
 if(playerPressed === 'Escape'){
     gameOver();
@@ -51,6 +64,9 @@ if(playerPressed === 'Escape'){
         // document.getElementById('score').innerText = newScore;
 
         // option-2
+        audio.src ="./sounds/correct.mp3";
+        audio.play();
+
         const currentScore = getCurrentPointById('score');
         const newScore = currentScore+1;
         setUpdatedPointById('score', newScore);
@@ -66,12 +82,21 @@ if(playerPressed === 'Escape'){
         // currentLifeElement.innerText = newLife;
 
         // option-2
+        audio.src ="./sounds/wrong.mp3";
+        audio.play();
         const currentLife = getCurrentPointById('life');
         const newLife = currentLife-1;
         setUpdatedPointById('life', newLife);
 
+        const colorPercentage = (newLife/5)*100;
+        const artBoard = document.getElementById('art-board');
+        artBoard.style.background= `linear-gradient(rgba(255,255,255,0.7) ${colorPercentage}%, red)`;
+        
+
         if(newLife === 0){
             gameOver();
+            audio.src ="./sounds/end.mp3";
+            audio.play();
         }
     }
 }
